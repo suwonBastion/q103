15618fe2600cb2b3373434331658f632fe08d493
msblastv2 <msblastv2@gmail.com>
2020-09-22 08:43:21 +0000

    Initial commit

---
+++ Id.java
@@ -0,0 +1,28 @@
+class Id{
+
+  private static int counter = 0;
+  private int id;
+
+  public Id() {
+    id = ++counter;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public static int getMaxId() {
+    return counter;
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
